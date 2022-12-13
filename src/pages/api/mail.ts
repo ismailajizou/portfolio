9bff890ac1ddc68edba3502d3b21c8159583b0e1
ismailajizou <ismailajizoumo@gmail.com>
2022-12-13 11:20:27 +0100

    fix email issue

--- src/pages/api/mail.ts
+++ src/pages/api/mail.ts
@@ -1,13 +1,17 @@
 import { env } from '@/env/server.mjs';
 import { mailSchema } from '@/utils/schemas/mail.schema';
-import { MailtrapClient } from 'mailtrap';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { i18n } from 'next-i18next';
+import nodemailer from 'nodemailer';
 import { z } from 'zod';
 
-const client = new MailtrapClient({
-  endpoint: env.MAILTRAP_ENDPOINT,
-  token: env.MAILTRAP_TOKEN,
+const transport = nodemailer.createTransport({
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: env.MAILTRAP_USER,
+    pass: env.MAILTRAP_PASS,
+  },
 });
 
 export default async function handler(
@@ -18,12 +22,11 @@ export default async function handler(
     return res.status(404).json({ message: i18n?.t('extra.404') });
   try {
     const { name, email, subject, body } = mailSchema.parse(req.body);
-    await client.send({
-      from: { email, name },
-      to: [{ email: env.MAIL_RECEIVER }],
+    await transport.sendMail({
+      from: { address: email, name },
+      to: env.MAIL_RECEIVER,
       subject,
       text: body,
-      category: 'contact',
     });
     res.status(200).json({ message: i18n?.t('extra.email-sent') });
   } catch (err) {
